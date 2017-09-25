011f88bf97a89aea0b4347eed1162a6bd81defc4
AnthonySorise <contact@anthonysorise.com>
2017-09-25 01:16:23 -0700

    Parenthesis Added and CE improvements

--- main.js
+++ main.js
@@ -7,70 +7,173 @@ var calcInput = [];
 var numInputInitiated = false;
 var total = 0;
 var lastButtonPressedWasEqual = false;
+var lastButtonPressedWasCE = false;
 var pressedCEafterEqual = false;
 var orderOfOperationMode = true;
+var parenthesisToClose = 0;
+
+$(window).keydown(function(event){
+    switch(event.key) {
+        case "0":
+            $("#num_0").addClass("active");
+            break;
+        case "1":
+            $("#num_1").addClass("active");
+            break;
+        case "2":
+            $("#num_2").addClass("active");
+            break;
+        case "3":
+            $("#num_3").addClass("active");
+            break;
+        case "4":
+            $("#num_4").addClass("active");
+            break;
+        case "5":
+            $("#num_5").addClass("active");
+            break;
+        case "6":
+            $("#num_6").addClass("active");
+            break;
+        case "7":
+            $("#num_7").addClass("active");
+            break;
+        case "8":
+            $("#num_8").addClass("active");
+            break;
+        case "9":
+            $("#num_9").addClass("active");
+            break;
+        case "+":
+            $("#op_add").addClass("active");
+            break;
+        case "-":
+            $("#op_subtract").addClass("active");
+            break;
+        case "*":
+            $("#op_multiply").addClass("active");
+            break;
+        case "/":
+            $("#op_divide").addClass("active");
+            break;
+        case ".":
+            $("#decimal").addClass("active");
+            break;
+        case "Backspace":
+            $("#clear_c").addClass("active");
+            break;
+        case "Delete":
+            $("#clear_ce").addClass("active");
+            break;
+        case "(":
+            $("#parenthesisLeft").addClass("active");
+            break;
+        case ")":
+            $("#parenthesisRight").addClass("active");
+            break;
+        case "Enter":
+            $("#op_equal").addClass("active");
+            break;
+        case "=":
+            $("#op_equal").addClass("active");
+            break;
+        case "Pause":
+            $("#negativeButton").addClass("active");
+            break;
+        case "End":
+            $("#negativeButton").addClass("active");
+            break;
+        case "F9":  //what windows calc uses
+            $("#negativeButton").addClass("active");
+            break;
+        case "Home":  //what windows calc uses
+            $("#orderOfOperationSwitch").addClass("active");
+            break;
+    }
+});
 
 $(window).keyup(function(event){
     switch(event.key) {
         case "0":
-            $("#num_0").click();
+            $("#num_0").click().removeClass("active");
             break;
         case "1":
-            $("#num_1").click();
+            $("#num_1").click().removeClass("active");
             break;
         case "2":
-            $("#num_2").click();
+            $("#num_2").click().removeClass("active");
             break;
         case "3":
-            $("#num_3").click();
+            $("#num_3").click().removeClass("active");
             break;
         case "4":
-            $("#num_4").click();
+            $("#num_4").click().removeClass("active");
             break;
         case "5":
-            $("#num_5").click();
+            $("#num_5").click().removeClass("active");
             break;
         case "6":
-            $("#num_6").click();
+            $("#num_6").click().removeClass("active");
             break;
         case "7":
-            $("#num_7").click();
+            $("#num_7").click().removeClass("active");
             break;
         case "8":
-            $("#num_8").click();
+            $("#num_8").click().removeClass("active");
             break;
         case "9":
-            $("#num_9").click();
+            $("#num_9").click().removeClass("active");
             break;
         case "+":
-            $("#op_add").click();
+            $("#op_add").click().removeClass("active");
             break;
         case "-":
-            $("#op_subtract").click();
+            $("#op_subtract").click().removeClass("active");
             break;
         case "*":
-            $("#op_multiply").click();
+            $("#op_multiply").click().removeClass("active");
             break;
         case "/":
-            $("#op_divide").click();
+            $("#op_divide").click().removeClass("active");
             break;
         case ".":
-            $("#decimal").click();
+            $("#decimal").click().removeClass("active");
             break;
         case "Backspace":
-            $("#clear_c").click();
+            $("#clear_c").click().removeClass("active");
             break;
         case "Delete":
-            $("#clear_ce").click();
+            $("#clear_ce").click().removeClass("active");
+            break;
+        case "(":
+            $("#parenthesisLeft").click().removeClass("active");
+            break;
+        case ")":
+            $("#parenthesisRight").click().removeClass("active");
             break;
         case "Enter":
-            $("#op_equal").click();
+            $("#op_equal").click().removeClass("active");
+            break;
+        case "=":
+            $("#op_equal").click().removeClass("active");
+            break;
+        case "Pause":
+            $("#negativeButton").click().removeClass("active");
+            break;
+        case "End":
+            $("#negativeButton").click().removeClass("active");
+            break;
+        case "F9":  //what windows calc uses
+            $("#negativeButton").click().removeClass("active");
+            break;
+        case "Home":  //what windows calc uses
+            $("#orderOfOperationSwitch").click().removeClass("active");
             break;
     }
 });
 
 function display(toDisplay){
-    if(isNaN(toDisplay) === true && toDisplay !== "-"){
+    if(isNaN(toDisplay) === true && (toDisplay !== "-" && toDisplay !== "(" && toDisplay !== ")")){
         toDisplay = "ERROR";
     }
     $("#display").find("p").text(toDisplay)
@@ -79,6 +182,8 @@ function display(toDisplay){
 function applyClickHandlers(){
     $(".numberButton").on("click", handleNumber);
     $("#negativeButton").on("click", handleNegative);
+    $("#parenthesisLeft").on("click", handleParenthesisLeft);
+    $("#parenthesisRight").on("click", handleParenthesisRight);
     $(".operatorButton").on("click", handleOperator);
     $("#op_equal").on("click", handleEqual);
     $("#clear_ce").on("click", handleCE);
@@ -107,24 +212,32 @@ function handleNumber() {
     }
     if (!numInputInitiated) {
         numInputInitiated = true;
+        if(input === "."){
+            input = "0."
+        }
         calcInput.push(input);
     }
     else{
-        calcInput[calcInput.length - 1] += input;
+        if(calcInput[calcInput.length-1] === "0"){
+            calcInput.pop();
+            calcInput.push(input)
+        }
+        else {
+            calcInput[calcInput.length - 1] += input;
+        }
     }
     display(calcInput[calcInput.length - 1]);
     console.log("Input: ", calcInput);
     lastButtonPressedWasEqual = false;
+    lastButtonPressedWasCE = false;
     pressedCEafterEqual = false;
 }
 
 function handleNegative() {
-    if (numInputInitiated === false) {
-        if (calcInput.length === 0 || lastButtonPressedWasOperator() === true) {
-            numInputInitiated = true;
-            calcInput.push("-");
-            display("-");
-        }
+    if (numInputInitiated === false && (calcInput.length === 0 || lastButtonPressedWasOperator() === true)) {
+        numInputInitiated = true;
+        calcInput.push("-");
+        display("-");
     }
     else if(lastButtonPressedWasEqual === false){
         if (calcInput[calcInput.length - 1] === "-") {
@@ -144,14 +257,43 @@ function handleNegative() {
         display(total);
     }
     console.log("Input: ", calcInput);
+    lastButtonPressedWasEqual = false;
+    lastButtonPressedWasCE = false;
+    pressedCEafterEqual = false;
+}
+
+function handleParenthesisLeft(){
+    if (numInputInitiated === false && (calcInput.length === 0 || lastButtonPressedWasOperator() === true)) {
+        calcInput.push("(");
+        display("(");
+        parenthesisToClose += 1;
+        lastButtonPressedWasEqual = false;
+        lastButtonPressedWasCE = false;
+        pressedCEafterEqual = false;
+        console.log("Input: ", calcInput)
+    }
+
+}
+function handleParenthesisRight(){
+    if(parenthesisToClose > 0 && calcInput[calcInput.length-1] !== "("){
+        calcInput.push(")");
+        display(")");
+        parenthesisToClose -= 1;
+        lastButtonPressedWasEqual = false;
+        lastButtonPressedWasCE = false;
+        pressedCEafterEqual = false;
+        console.log("Input: ", calcInput)
+    }
+
 }
 
 function handleOperator(){
-
+    var operator = $(this).find("p").text().toString();
     if(pressedCEafterEqual === true){
         handleC();
+        calcInput[0] = 0;
+        calcInput.push(operator);
     }
-    var operator = $(this).find("p").text().toString();
     if(calcInput.length === 0) {
         calcInput[0] = 0;
     }
@@ -166,6 +308,7 @@ function handleOperator(){
     console.log("Input: ", calcInput);
     numInputInitiated = false;
     lastButtonPressedWasEqual = false;
+    lastButtonPressedWasCE = false;
     pressedCEafterEqual = false;
 }
 
@@ -202,55 +345,111 @@ function calculatePair(operatorIndex, inputArray){
 
 function calculateEquation(){
     if(calcInput.length > 2) {
-        if(calcInput[calcInput.length-1] === ""){
-            calcInput[calcInput.length-1] = 0;
-        }
         var equationToSolve = [];
         for (var i = 0; i < calcInput.length; i++) {    //make clone of calcInput to preserve it
             equationToSolve.push(calcInput[i])
         }
         var lastValueInEquationToSolve = equationToSolve[equationToSolve.length-1];
+        if(lastValueInEquationToSolve === "("){
+            equationToSolve.pop();
+        }
+        lastValueInEquationToSolve = equationToSolve[equationToSolve.length-1];
         if(lastValueInEquationToSolve === "+" || lastValueInEquationToSolve === "-" || lastValueInEquationToSolve === "x" || lastValueInEquationToSolve === "÷" ){
             equationToSolve.pop();
         }
         if(orderOfOperationMode) {
-            while (equationToSolve.indexOf("x") !== -1 || equationToSolve.indexOf("÷") !== -1) {
-                for (var i = 0; i < equationToSolve.length; i++) {
-                    if (equationToSolve[i] === "x" || equationToSolve[i] === "÷") {
-                        calculatePair(i, equationToSolve);
-                        i = i - 1; //compensates for calculatePair() - which removes from array.
-                    }
-                }
-                console.log("Solve Multiplication/Division: ", equationToSolve);
-            }
-            while (equationToSolve.indexOf("+") !== -1 || equationToSolve.indexOf("-") !== -1) {
-                for (var i = 0; i < equationToSolve.length; i++) {
-                    if (equationToSolve[i] === "+" || equationToSolve[i] === "-") {
-                        calculatePair(i, equationToSolve);
-                        i = i - 1;  //compensates for calculatePair() - which removes from array.
-                    }
-                }
-                console.log("Solve Addition/Subtraction: ", equationToSolve)
-            }
+            solveEquationWithOrderOfOperation(equationToSolve, true)
         }
         else{
-            while (equationToSolve.length > 1){
-                for (var i = 0; i < equationToSolve.length; i++) {
-                    if (equationToSolve[i] === "x" || equationToSolve[i] === "÷" || equationToSolve[i] === "+" || equationToSolve[i] === "-") {
-                        calculatePair(i, equationToSolve);
-                        i = i - 1; //compensates for calculatePair() - which removes from array.
-                    }
-                }
-            }
-            console.log("Solve: ", equationToSolve)
+            solveEquationWithSuccessiveOperation(equationToSolve)
         }
     }
+}
+
+function solveEquationWithOrderOfOperation(equationToSolve, logToConsole){
+    while (equationToSolve.indexOf("(") !== -1 || equationToSolve.indexOf(")") !== -1) {
+        var leftParenIndexes = [];
+        var rightParenIndexes = [];
+        for (var i = 0; i < equationToSolve.length; i++){
+            if(equationToSolve[i] === "("){
+                leftParenIndexes.push(i);
+            }
+            if(equationToSolve[i] === ")"){
+                rightParenIndexes.push(i);
+            }
+        }
+        if (rightParenIndexes.length < leftParenIndexes.length){    //close parenthesis if it hasn't been done already
+            equationToSolve.push(")");
+            rightParenIndexes.push(equationToSolve.length-1)
+        }
+        //get the inner parenthesis - last left, first right
+        //do regular order of operation (copy and paste?)
+        //remove the used parenthesis, and equation, replace with total
+        //console log
+        var lastLeftParen = leftParenIndexes[leftParenIndexes.length-1];
+        var firstRightParen = rightParenIndexes[0];
+        var parenEquation = [];
+        for(var i = lastLeftParen + 1; i < firstRightParen; i++){
+            parenEquation.push(equationToSolve[i]);
+        }
+        equationToSolve.splice(firstRightParen, 1);
+        equationToSolve.splice(lastLeftParen, firstRightParen-lastLeftParen, solveEquationWithOrderOfOperation(parenEquation, false)[0]);
+        console.log("Solve Parentheses: ", equationToSolve)
+    }
+    while (equationToSolve.indexOf("x") !== -1 || equationToSolve.indexOf("÷") !== -1) {
+        for (var i = 0; i < equationToSolve.length; i++) {
+            if (equationToSolve[i] === "x" || equationToSolve[i] === "÷") {
+                calculatePair(i, equationToSolve);
+                i = i - 1; //compensates for calculatePair() - which removes from array.
+            }
+        }
+        if(logToConsole === true) {
+            console.log("Solve Multiplication/Division: ", equationToSolve);
+        }
+    }
+    while (equationToSolve.indexOf("+") !== -1 || equationToSolve.indexOf("-") !== -1) {
+        for (var i = 0; i < equationToSolve.length; i++) {
+            if (equationToSolve[i] === "+" || equationToSolve[i] === "-") {
+                calculatePair(i, equationToSolve);
+                i = i - 1;  //compensates for calculatePair() - which removes from array.
+            }
+        }
+        if(logToConsole === true) {
+            console.log("Solve Addition/Subtraction: ", equationToSolve)
+        }
+    }
+    return equationToSolve;
+}
+function solveEquationWithSuccessiveOperation(equationToSolve){
+    for (var i = 0; i < equationToSolve.length; i++){   //remove parenthesis
+        if(equationToSolve[i] === "("){
+            equationToSolve.splice(i, 1);
+        }
+        if(equationToSolve[i] === ")"){
+            equationToSolve.splice(i, 1);
+        }
+    }
+    while (equationToSolve.length > 1){
+        for (var i = 0; i < equationToSolve.length; i++) {
+            if (equationToSolve[i] === "x" || equationToSolve[i] === "÷" || equationToSolve[i] === "+" || equationToSolve[i] === "-") {
+                calculatePair(i, equationToSolve);
+                i = i - 1; //compensates for calculatePair() - which removes from array.
+            }
+        }
+    }
+    console.log("Solve: ", equationToSolve)
+    return equationToSolve;
 }
 
 function handleEqual(){
     if(calcInput.length > 1) {
         if (lastButtonPressedWasEqual) {
             operationRepeat();
+        }
+        if(lastButtonPressedWasCE){
+            if(lastButtonPressedWasOperator()){
+                calcInput.push(0)
+            }
         }
         if (lastButtonPressedWasOperator() === true) {
             if(calcInput.length === 2){
@@ -262,6 +461,7 @@ function handleEqual(){
         }
         calculateEquation();
         lastButtonPressedWasEqual = true;
+        lastButtonPressedWasCE = false;
         pressedCEafterEqual = false;
     }
 }
@@ -281,26 +481,41 @@ function operationRollover(){
     calcInput.push(total.toString());
 }
 
-function handleCE(){
-    if(lastButtonPressedWasEqual){
-        pressedCEafterEqual = true;
+function handleCE() {
+    if(!lastButtonPressedWasCE) {
+        if(lastButtonPressedWasOperator() || calcInput[calcInput.length - 1] === "(" || calcInput[calcInput.length - 1] === ")"){
+            while (lastButtonPressedWasOperator() || calcInput[calcInput.length - 1] === "(" || calcInput[calcInput.length - 1] === ")") {
+                if (lastButtonPressedWasOperator() || calcInput[calcInput.length - 1] === "(" || calcInput[calcInput.length - 1] === ")") {
+                    calcInput.pop();
+                    console.log("Input: ", calcInput)
+                    lastButtonPressedWasEqual = false;
+                    lastButtonPressedWasCE = true;
+                    numInputInitiated = false;
+                }
+            }
+        }
+        else{
+            if (lastButtonPressedWasEqual) {
+                pressedCEafterEqual = true;
+                var lastOperator = calcInput[calcInput.length - 2];
+                var lastNumber = calcInput[calcInput.length - 1];
+                handleC();
+                calcInput[0] = 0;
+                calcInput[1] = lastOperator;
+                calcInput[2] = lastNumber;
+                console.log("Input: ", calcInput);
+                lastButtonPressedWasEqual = false;
+                lastButtonPressedWasCE = true;
+                return
+            }
+            calcInput.pop();
+            display(0);
+            console.log("Input: ", calcInput);
+            lastButtonPressedWasEqual = false;
+            lastButtonPressedWasCE = true;
+            numInputInitiated = false;
+        }
     }
-    if(lastButtonPressedWasEqual === true){
-        var lastOperator = calcInput[calcInput.length-2];
-        var lastNumber = calcInput[calcInput.length-1];
-        handleC();
-        calcInput[0] = 0;
-        calcInput[1] = lastOperator;
-        calcInput[2] = lastNumber;
-        console.log("Input: ", calcInput);
-        lastButtonPressedWasEqual = false;
-        return
-    }
-    calcInput.pop();
-    calcInput.push("");
-    display(0);
-    console.log("Input: ", calcInput);
-    lastButtonPressedWasEqual = false;
 }
 
 function handleC(){
@@ -309,6 +524,7 @@ function handleC(){
     var total = 0;
     display(total);
     lastButtonPressedWasEqual = false;
+    console.log("Input: ", calcInput)
 }
 
 function handleOrderOfOperationSwitch(){
